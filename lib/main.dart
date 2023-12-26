eb7cc136ecd5d01c0acf0867dea44ba34c1a00bf
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2023-12-26 10:51:59 +0200

    create customAppbar

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,32 @@ class NoteApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: SafeArea(
+        child: Scaffold(
+          body: CustomAppbar(),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomAppbar extends StatelessWidget {
+  const CustomAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text(
+          "Note App",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+      ],
+    );
   }
 }
